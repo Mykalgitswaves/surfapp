e825afe54a11cf0113b6f7f7b96246c612114535
Mykalgitswaves <michaelfinal.png@gmail.com>
2023-03-12 21:13:19 -0400

    Correct for loop js

--- front-end/src/components/TheWelcome.vue
+++ front-end/src/components/TheWelcome.vue
@@ -1,7 +1,7 @@
 <template>
   <div> TODO: THis whole section i guess lol. Whelp 🫥
-    <div v-for="obj in buoyData" :key="obj"> 
-      <div v-for="data in obj" :key="data">
+    <div v-for="(obj, index) in buoyData" :key="index"> 
+      <div v-for="(data, index) in obj" :key="index">
          {{ data.swh }} {{ data.swp }} {{ `${data.time[0]}  ${data.time[1]}  ${data.time[2]}  ${data.time[3]}  ${data.time[4]}` }} 
       </div>
     </div>
